176aba3580ddb02174a781b233817830a63a4215
Diogo Kollross <diogoko@gmail.com>
2013-06-20 22:14:21 -0300

    first commit

---
+++ RsiAlarm/GlobalHook.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Copyright 2013 Diogo Kollross
+ *
+ * This file is part of RsiAlarm.
+ *
+ * RsiAlarm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RsiAlarm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RsiAlarm. If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace RsiAlarm
+{
+    public class GlobalHook
+    {
+        private const int WH_KEYBOARD_LL = 13;
+
+        private const int WM_KEYDOWN = 0x0100;
+
+        private IntPtr HookID = IntPtr.Zero;
+
+        private LowLevelKeyboardProc HookProc;
+
+        public void Set()
+        {
+            if (HookID != IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Global hook is already set");
+            }
+
+            HookProc = new LowLevelKeyboardProc(HookCallback);
+            HookID = SetHook(HookProc);
+        }
+
+        public void Unset()
+        {
+            if (HookID == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Global hook is not set");
+            }
+
+            UnhookWindowsHookEx(HookID);
+            HookID = IntPtr.Zero;
+            HookProc = null;
+        }
+
+        public event EventHandler EventTriggered;
+
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                if (EventTriggered != null)
+                {
+                    EventTriggered(this, new EventArgs());
+                }
+            }
+            return CallNextHookEx(HookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
---
+++ RsiAlarm/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿/*
+ * Copyright 2013 Diogo Kollross
+ *
+ * This file is part of RsiAlarm.
+ *
+ * RsiAlarm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RsiAlarm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RsiAlarm. If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Media.Animation;
+using System.Configuration;
+using System.Reflection;
+
+namespace RsiAlarm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GlobalHook KeyboardHook;
+
+        private long KeyboardPoints = 0;
+
+        private Stopwatch KeyboardStopwatch;
+
+        private long KeyboardPointsWarningLimit;
+
+        private long KeyboardPointsDecreaseRate;
+
+        private int FadeInDuration;
+
+        private int FadeOutDuration;
+
+        private int LoadIntSetting(string key, int defaultValue)
+        {
+            if (ConfigurationManager.AppSettings[key] != null)
+            {
+                return int.Parse(ConfigurationManager.AppSettings[key]);
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+
+        public MainWindow()
+        {
+            KeyboardPointsWarningLimit = LoadIntSetting("KeyboardPointsWarningLimit", 15);
+            KeyboardPointsDecreaseRate = LoadIntSetting("KeyboardPointsDecreaseRate", 5);
+            FadeInDuration = LoadIntSetting("FadeInDuration", 50);
+            FadeOutDuration = LoadIntSetting("FadeOutDuration", 300);
+
+            InitializeComponent();
+
+            KeyboardHook = new GlobalHook();
+            KeyboardHook.EventTriggered += KeyboardHook_EventTriggered;
+            KeyboardHook.Set();
+
+            KeyboardStopwatch = Stopwatch.StartNew();
+
+            DoubleAnimation fadeIn = new DoubleAnimation();
+            fadeIn.From = 0.0;
+            fadeIn.To = 1.0;
+            fadeIn.Duration = new Duration(TimeSpan.FromMilliseconds(FadeInDuration));
+
+            DoubleAnimation fadeOut = new DoubleAnimation();
+            fadeOut.From = 1.0;
+            fadeOut.To = 0.0;
+            fadeOut.Duration = new Duration(TimeSpan.FromMilliseconds(FadeOutDuration));
+
+            WarningStoryboard = new Storyboard();
+            WarningStoryboard.Children.Add(fadeIn);
+            WarningStoryboard.Children.Add(fadeOut);
+            WarningStoryboard.Completed += WarningStoryboard_Completed;
+
+            Storyboard.SetTarget(fadeIn, WarningRectangle);
+            Storyboard.SetTargetProperty(fadeIn, new PropertyPath(Rectangle.OpacityProperty));
+
+            Storyboard.SetTarget(fadeOut, WarningRectangle);
+            Storyboard.SetTargetProperty(fadeOut, new PropertyPath(Rectangle.OpacityProperty));
+        }
+
+        private void WarningStoryboard_Completed(object sender, EventArgs e)
+        {
+            Visibility = Visibility.Hidden;
+        }
+
+        private void KeyboardHook_EventTriggered(object sender, EventArgs e)
+        {
+            KeyboardPoints++;
+
+            long elapsed = KeyboardStopwatch.ElapsedMilliseconds;
+            if (elapsed >= 1000)
+            {
+                long pointsToRemove = elapsed * KeyboardPointsDecreaseRate / 1000;
+                KeyboardPoints = Math.Max(KeyboardPoints - pointsToRemove, 0);
+
+                KeyboardStopwatch.Restart();
+            }
+
+            if (KeyboardPoints >= KeyboardPointsWarningLimit)
+            {
+                Visibility = Visibility.Visible;
+
+                WarningStoryboard.Begin();
+                KeyboardPoints = 0;
+            }
+        }
+
+        private Storyboard WarningStoryboard;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Width = SystemParameters.PrimaryScreenWidth;
+            Height = SystemParameters.CaptionHeight;
+        }
+    }
+}
